4c9c6eb7c4f6002f67184d53e42bc79c2d67a4ca
Darboux12 <dariuszbula98@gmail.com>
2023-11-17 14:44:34 +0100

    Add basic osv integration

--- api/src/main/java/pl/dockerguardimage/api/OsvApiController.java
+++ api/src/main/java/pl/dockerguardimage/api/OsvApiController.java
@@ -1,6 +1,6 @@
 package pl.dockerguardimage.api;
 
-import jakarta.validation.Valid;
+import jakarta.validation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +13,7 @@ import pl.dockerguardimage.core.osv.dto.request.SystemRequestService;
 import pl.dockerguardimage.core.osv.dto.response.SystemResponseService;
 
 import java.io.IOException;
+import java.util.Set;
 
 @RestController()
 public class OsvApiController {
@@ -25,9 +26,20 @@ public class OsvApiController {
 
     @PostMapping("/package/vulnerability")
     public ResponseEntity<SystemResponse> findPackageVulnerability(@Valid @RequestBody SystemRequest systemRequest) throws IOException, InterruptedException {
+
+        System.out.println(systemRequest);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<SystemRequest>> violations = validator.validate(systemRequest);
+
+        System.out.println(violations);
+
+
+
         SystemRequestService requestService = OsvApiMapperController.mapSystemRequestToServiceRequest(systemRequest);
         SystemResponseService responseService = osvApiService.findVulnerabilitiesFromOsvApi(requestService);
-        SystemResponse response = OsvApiMapperController.mapServiceResponseToSystemResponse(responseService);
+        SystemResponse response= OsvApiMapperController.mapServiceResponseToSystemResponse(responseService);
         return ResponseEntity.ok(response);
     }
 }
